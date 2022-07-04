93eeb9eff781d04d6098f72af3d85fab793ff57c
naoki-kkc <naoki2kuchi@gmail.com>
2022-07-05 02:22:36 +0900

    modify code

--- pythontutorialnet/section_01/05_Command_Binding/tkinter_button_command_arguments.py
+++ pythontutorialnet/section_01/05_Command_Binding/tkinter_button_command_arguments.py
@@ -6,8 +6,14 @@ def recieve_arg(option):
 
 root = tk.Tk()
 
+# 引数を渡す場合はcommandでラムダ式を利用
 ttk.Button(root, text='hoge', command=lambda: recieve_arg('hoge')).pack()
 ttk.Button(root, text='fuga', command=lambda: recieve_arg('fuga')).pack()
 ttk.Button(root, text='foobar', command=lambda: recieve_arg('foobar')).pack()
+
+# 注意：
+# ・commandによるバインディングは一部のウィジェットでしか利用できない
+# ・ボタンのcommandはreturnにバインドされない
+# これらは後述のイベントバインディングで対応する
 
 root.mainloop()
